3f3d2647f1df59c573d0a6646e786ffcaabc9bb7
Michael Donovan <mike@eggborne.com>
2018-10-12 10:03:14 -0700

    Split app.js into two files

--- src/js/control.js
+++ src/js/control.js
@@ -1,44 +1,88 @@
-
-
-
-
-
-// export function touchStart(event) {
-// 	for (var i=0; i<event.changedTouches.length; i++) {
-// 		var newTouch = event.changedTouches[i]
-// 		app.currentTouches.push(new Touch(app, newTouch));
-// 	}
-// }
-// export function touchMove(event) {
-// 	var movingTouches = [];
-// 	for (var i=0; i<event.changedTouches.length; i++) {
-//     movingTouches.push(copyTouch(event.changedTouches[i]));
-// 	}
-// 	movingTouches.forEach(function(touchEvent, i) {
-// 		var touchObject = new Touch(app, touchEvent);
-// 		// take each touch that moved...
-// 		app.currentTouches.forEach(function (existingTouch, j) {
-// 			//...find it in the list...
-// 			if (touchObject.identifier === existingTouch.identifier) {
-// 				//...replace the old {x,y} with the new one
-// 				app.currentTouches[j].x = touchObject.x;
-// 				app.currentTouches[j].y = touchObject.y;
-// 				// check if it has completed a swipe
-// 				var swiped = app.currentTouches[j].getSwipe()
-// 				app.swipeActions[swiped]()
-// 			}
-// 		});
-// 	});
-// }
-// export function touchEnd(event) {
-// 	Array.from(event.changedTouches).forEach(function(touch, i) {
-// 		var duration = app.currentTouches[i].getDuration();
-// 		var distance = app.currentTouches[i].getDistance();
-// 		if (duration <= app.tapTime 
-// 			&& distance.x <= app.tapDistance && distance.y < app.tapDistance) {
-// 				console.log("TAPPED!")
-// 		}
-// 		app.currentTouches.splice(i, 1);
-// 	});
-	
-// }
+export function Touch(app,touchEvent) {
+	this.identifier = touchEvent.identifier;
+  this.startTime = Date.now();
+	this.x = Math.round(touchEvent.pageX);
+	this.y = Math.round(touchEvent.pageY);
+  this.startSpot = { x: this.x, y: this.y };
+  this.endSpot = {};
+	this.getDuration = function () {
+		return Date.now() - this.startTime;
+	}
+	this.getDistance = function () {
+    var distance = {};
+		distance.x = this.x - this.startSpot.x;
+		distance.y = this.y - this.startSpot.y;
+		return distance
+	}
+	this.getSwipe = function () {
+		var fullSwipe = ""
+		var duration = this.getDuration();
+		var distance = this.getDistance();
+		if (distance.y <= app.gestures.swipe.north.distance && duration <= app.gestures.swipe.north.duration) {
+			fullSwipe += "north";
+		}
+		if (distance.y >= app.gestures.swipe.south.distance && duration <= app.gestures.swipe.south.duration) {
+			fullSwipe += "south";
+		}
+		if (distance.x <= app.gestures.swipe.west.distance && duration <= app.gestures.swipe.west.duration) {
+			fullSwipe += "west";
+		}
+		if (distance.x >= app.gestures.swipe.east.distance && duration <= app.gestures.swipe.east.duration) {
+			fullSwipe += "east";
+		}
+    return fullSwipe;
+	}
+}
+export function TouchHandler(app) {
+	console.log("handler initialized!")
+	this.touchStart = function(event) {
+		for (var i=0; i<event.changedTouches.length; i++) {
+			var newTouch = event.changedTouches[i]
+			app.currentTouches.push(new Touch(app, newTouch));
+		}
+	}
+	this.touchMove = function(event) {
+		var movingTouches = [];
+		for (var i=0; i<event.changedTouches.length; i++) {
+      var movingTouch = event.changedTouches[i];
+      var touchCopy = { 'identifier': movingTouch.identifier, 'pageX': movingTouch.pageX, 'pageY': movingTouch.pageY }
+			movingTouches.push(touchCopy);
+		}
+		movingTouches.forEach(function(touchEvent, i) {
+			var touchObject = new Touch(app, touchEvent);
+			// take each touch that moved...
+			app.currentTouches.forEach(function (existingTouch, j) {
+				//...find it in the list...
+				if (touchObject.identifier === existingTouch.identifier) {
+					//...replace the old {x,y} with the new one
+					app.currentTouches[j].x = touchObject.x;
+					app.currentTouches[j].y = touchObject.y;
+					// check if it has completed a swipe
+          var swiped = app.currentTouches[j].getSwipe()
+          if (swiped) {
+            app.swipeActions[swiped]()
+          }
+				}
+			});
+		});
+	}
+	this.touchEnd = function(event) {
+		Array.from(event.changedTouches).forEach(function(touch, i) {
+			var duration = app.currentTouches[i].getDuration();
+      var distance = app.currentTouches[i].getDistance();
+			if (duration <= app.gestures.tap.time
+				&& distance.x <= app.gestures.tap.distance && distance.y < app.gestures.tap.distance) {
+					$("#message").html("TAPPED!")
+			} else {
+        $("#message").html("TOO SLOW! " + duration)
+      }
+			app.currentTouches.splice(i, 1);
+		});
+  }
+  this.setInputs = function() {
+    document.body.addEventListener('touchstart',this.touchStart,true);
+    document.body.addEventListener('touchmove',this.touchMove,true);
+    document.body.addEventListener('touchend',this.touchEnd,true);
+    console.log("inputs set!")
+  }
+}
